55f177fbbc2ace3664671068ee315e59d38b02e4
Simon-Cobb <39433757+Simon-Cobb@users.noreply.github.com>
2019-02-28 07:31:10 +0000

    Update mdpi.js

--- Copyright app/mdpi.js
+++ Copyright app/mdpi.js
@@ -1,18 +1,21 @@
+//MDPI
 window.onload = function() {myFunction()};
 function myFunction() {
-if(document.URL.indexOf("/htm#")==-1)
+if(document.URL.indexOf("#")==-1)
         {
-			  window.open(window.location.href + "/htm#", "_self")
+			  window.open(window.location.href + "/htm#")
         }
 }
 	var node1 = document.createElement("p");
 	node1.id = "copyleft";
-var cardId1 = document.querySelector("setion#html-copyright").innerText;
-	var text1 = document.createTextNode(cardId1);
+var cr = document.querySelector("setion#html-copyright").innerText;
+	var text1 = document.createTextNode(cr);
 	node1.appendChild(text1);
 	node1.style.cssText="line-height:1.2;font-size:10px;color:#ff0000;border:1px solid #0000ff;background-color:#f0f0f0;"
-document.querySelector("div#html-article-title").appendChild(node1);
 
+	var h = document.querySelector("div#html-article-title");
+    h.insertBefore(node1, h.childNodes[0])
+	
 var txt = document.getElementById("copyleft").innerText;
 chrome.storage.local.set({key: txt}, function() {
           console.log('Copyright statement set to: ' + txt);
