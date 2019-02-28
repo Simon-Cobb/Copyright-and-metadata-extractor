b4e358085b9f58d8856b5c407aa336d1989e4ab6
Simon-Cobb <39433757+Simon-Cobb@users.noreply.github.com>
2019-02-28 07:34:37 +0000

    Update plos.js

--- Copyright app/plos.js
+++ Copyright app/plos.js
@@ -1,8 +1,8 @@
+//PLOS
 var str = document.querySelector(".articleinfo").innerText;
 var res = str.split("Copyright: ")[1].split(/\n/)[0];
-var cr = res;
-if (cr !== null) {
-	var copyright = cr;
+if (res !== null) {
+	var copyright = res;
 }
 
 	var node1 = document.createElement("p");
@@ -12,8 +12,9 @@ if (cr !== null) {
 	node1.appendChild(text);
 	node1.style.cssText="line-height:1.2;font-size:10px;color:#ff0000;border:1px solid #0000ff;background-color:#f0f0f0;"
 	
-document.querySelector("h1#artTitle").appendChild(node1);
-
+var h = document.querySelector("h1#artTitle");
+    h.insertBefore(node1, h.childNodes[0]);
+	
 var txt = document.getElementById("copyleft").innerText;
 chrome.storage.local.set({key: txt}, function() {
           console.log('Copyright statement set to: ' + txt);
