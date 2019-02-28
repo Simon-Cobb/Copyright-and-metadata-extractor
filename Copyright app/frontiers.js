e0b124f90afcc04d9de1fc272c225bde301994ef
Simon-Cobb <39433757+Simon-Cobb@users.noreply.github.com>
2019-02-28 07:25:46 +0000

    Update frontiers.js

--- Copyright app/frontiers.js
+++ Copyright app/frontiers.js
@@ -1,16 +1,19 @@
-var str = document.querySelector(".AbstractSummary p:nth-child(6)").innerText;
-var res = str.replace("Copyright ©", "©");
-var card_id1 = res;
-if (card_id1 !== null) {
-	var cardId1 = card_id1;
+//Frontiers
+var str = document.querySelector(".AbstractSummary").innerText;
+var res = str.split("Copyright")[1].split('*')[0].trim().replace(/^: /,'');
+
+if (res !== null) {
+	var copyright = res;
 }
 	var node1 = document.createElement("p");
 	node1.id = "copyleft";
-	var text1 = document.createTextNode(cardId1,"color: green; font-size: large");
+	var text1 = document.createTextNode(copyright);
 	
 	node1.appendChild(text1);
 	node1.style.cssText="line-height:1.2;font-size:10px;color:#ff0000;border:1px solid #0000ff;background-color:#f0f0f0;"
-document.querySelector(".JournalAbstract h1").appendChild(node1);
+
+var h = document.querySelector(".JournalAbstract h1");
+    h.insertBefore(node1, h.childNodes[0]);
 
 var txt = document.getElementById("copyleft").innerText;
 chrome.storage.local.set({key: txt}, function() {
