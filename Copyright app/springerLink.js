edbc7727c9a00997ffd64aa172180b00394ffc6e
Simon-Cobb <39433757+Simon-Cobb@users.noreply.github.com>
2019-02-28 07:44:42 +0000

    Update springerLink.js

--- Copyright app/springerLink.js
+++ Copyright app/springerLink.js
@@ -1,17 +1,18 @@
-var gatheredNodes1 = document.querySelector("div.ArticleCopyright").innerText;
-   var card_id1 = gatheredNodes1;
-      console.log(card_id1);
-if (card_id1 !== null) {
-	var cardId1 = card_id1;
+//Springer - https:link.springer.com
+var str = document.querySelector("div.ArticleCopyright").innerText;
+if (str !== null) {
+	var copyright = str;
 }
 
-	var node1 = document.createElement("p");
+var node1 = document.createElement("p");
 	node1.id = "copyleft";
-	var title = document.querySelector("div.enumeration span").innerText;
-	var text1 = document.createTextNode(cardId1 + ". This is an author produced version of a paper published in " + title + ". Uploaded in accordance with the publisher's self-archiving policy.","color: green; font-size: large");
+var title = document.querySelector("div.enumeration span").innerText;
+var text1 = document.createTextNode(copyright + ". This is an author produced version of a paper published in " + title + ". Uploaded in accordance with the publisher's self-archiving policy.");
 	node1.appendChild(text1);
 	node1.style.cssText="line-height:1.2;font-size:10px;color:#ff0000;border:1px solid #0000ff;background-color:#f0f0f0;"
-document.querySelector(".MainTitleSection").appendChild(node1);
+
+var h = document.querySelector(".MainTitleSection");
+    h.insertBefore(node1, h.childNodes[0]);	
 
 var txt = document.getElementById("copyleft").innerText;
 chrome.storage.local.set({key: txt}, function() {
